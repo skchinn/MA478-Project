c0649cc0156055b0e80e84345ce35c64d6c1c8bb
robeyjorgensen <robey.jorgensen@gmail.com>
2022-04-13 15:09:50 -0400

    Testing

--- data_reduction.R
+++ data_reduction.R
@@ -1,11 +1,11 @@
 library(tidyverse)
-setwd("C:/Users/Robey/Desktop/MA478 Proj/MA478-Project")
+setwd("C:/Users/Robey/Desktop/MA478-Project")
 
 full <- read.csv(file = "used_cars_data.csv", stringsAsFactors = TRUE)
 
 set.seed(222)
 data <- full %>% 
-  sample_n(round(nrow(full)*0.001))
+  sample_n(round(nrow(full)*0.005))
 
 data %>% write.csv("subset_used_cars_data.csv")
 rm(full)
